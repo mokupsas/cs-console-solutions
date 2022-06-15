c41b297a0c547ebfe9a3de5410adbbcabb70f000
mokupsas <mockus.ri@gmail.com>
2022-06-15 13:32:39 +0300

    Added bookshelf project

---
+++ Bookshelf/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Bookshelf
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
