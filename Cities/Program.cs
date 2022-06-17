ba050fd7d858059e1279e15860af8a41ebfaab0c
mokupsas <mockus.ri@gmail.com>
2022-06-17 19:20:59 +0300

    Add new project for Cities problem

---
+++ Cities/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+/*
+
+    The data for the cities is in a file: name, area and population. The first row contains the country
+    name. Create a class City to store data for one city. Find the city for which
+    has the highest population density and how many people live in the cities of the country.
+
+    Extend the program with actions on two sets of cities in two countries. For each country
+    data are stored in separate files. Which country has the highest urban population and which has the highest
+    has the most densely populated city. Write down in a separate file all the cities in both lists
+    data for the cities with at least k inhabitants.
+
+*/
+
+namespace Cities
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
